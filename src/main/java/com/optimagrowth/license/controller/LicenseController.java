74855672b79b639805026ee6584b8c6db9793b55
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-06-16 21:08:06 +0200

    Implement mock CRUD endpoints

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -1,21 +1,36 @@
 package com.optimagrowth.license.controller;
 
 import com.optimagrowth.license.model.License;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/organization/{organizationId}/license")
 public class LicenseController {
 
     @GetMapping(value = "/{licenseId}")
-    public License getLicense(@PathVariable String organizationId, @PathVariable String licenseId) {
+    public ResponseEntity<License> getLicense(@PathVariable String organizationId, @PathVariable String licenseId) {
         License license = new License();
         license.setOrganizationId(organizationId);
         license.setLicenseId(licenseId);
         license.setDescription("test");
-        return license;
+        return ResponseEntity.ok(license);
+    }
+
+    @PostMapping
+    public ResponseEntity<License> createLicense(@PathVariable String organizationId, @RequestBody License license) {
+        license.setOrganizationId(organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<License> updateLicense(@PathVariable String organizationId, @RequestBody License license) {
+        license.setOrganizationId(organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @DeleteMapping(value = "{licenseId}")
+    public ResponseEntity deleteLicense(@PathVariable String organizationId, @PathVariable String licenseId) {
+        return ResponseEntity.noContent().build();
     }
 }
