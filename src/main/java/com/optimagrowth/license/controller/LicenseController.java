1bf28b8698a09806ab8ead7744202260a370b83f
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-06-16 21:43:09 +0200

    Add mock service for CRUD operations

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -1,6 +1,8 @@
 package com.optimagrowth.license.controller;
 
 import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,29 +10,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/v1/organization/{organizationId}/license")
 public class LicenseController {
 
+    @Autowired
+    private LicenseService licenseService;
+
     @GetMapping(value = "/{licenseId}")
     public ResponseEntity<License> getLicense(@PathVariable String organizationId, @PathVariable String licenseId) {
-        License license = new License();
-        license.setOrganizationId(organizationId);
-        license.setLicenseId(licenseId);
-        license.setDescription("test");
+        License license = licenseService.getLicense(organizationId, licenseId);
         return ResponseEntity.ok(license);
     }
 
     @PostMapping
-    public ResponseEntity<License> createLicense(@PathVariable String organizationId, @RequestBody License license) {
-        license.setOrganizationId(organizationId);
-        return ResponseEntity.ok(license);
+    public ResponseEntity<String> createLicense(@PathVariable String organizationId, @RequestBody License license) {
+        String msg = licenseService.createLicense(license, organizationId);
+        return ResponseEntity.ok(msg);
     }
 
     @PutMapping
-    public ResponseEntity<License> updateLicense(@PathVariable String organizationId, @RequestBody License license) {
-        license.setOrganizationId(organizationId);
-        return ResponseEntity.ok(license);
+    public ResponseEntity<String> updateLicense(@PathVariable String organizationId, @RequestBody License license) {
+        String msg = licenseService.updateLicense(license, organizationId);
+        return ResponseEntity.ok(msg);
     }
 
     @DeleteMapping(value = "{licenseId}")
     public ResponseEntity deleteLicense(@PathVariable String organizationId, @PathVariable String licenseId) {
-        return ResponseEntity.noContent().build();
+        String msg = licenseService.deleteLicense(organizationId, licenseId);
+        return ResponseEntity.ok(msg);
     }
 }
