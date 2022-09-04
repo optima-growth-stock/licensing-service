2a8744196d338e912fcef348c883f1e9775c85f3
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-09-05 01:01:53 +0200

    Propagete correlation id and authentication token using UserContextInterceptor

--- src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -1,14 +1,18 @@
 package com.optimagrowth.license;
 
+import com.optimagrowth.license.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 @SpringBootApplication
@@ -36,7 +40,13 @@ public class LicensingServiceApplication {
     @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        final RestTemplate template = new RestTemplate();
+
+        List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+        interceptors.add(new UserContextInterceptor());
+        template.setInterceptors(interceptors);
+
+        return template;
     }
 
 }
