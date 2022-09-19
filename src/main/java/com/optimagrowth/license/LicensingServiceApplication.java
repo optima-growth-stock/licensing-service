fc0bc0e6bfc6d66a5cb4b3a0e2c0bf96fe1d1dc2
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-09-19 18:59:35 +0200

    Read organization change from kafka

--- src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -1,9 +1,15 @@
 package com.optimagrowth.license;
 
+import com.optimagrowth.license.events.model.OrganizationChangeModel;
 import com.optimagrowth.license.utils.UserContextInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -16,7 +22,10 @@ import java.util.List;
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class LicensingServiceApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(LicensingServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(LicensingServiceApplication.class, args);
@@ -47,6 +56,11 @@ public class LicensingServiceApplication {
         template.setInterceptors(interceptors);
 
         return template;
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void loggerSink(OrganizationChangeModel orgChange) {
+        logger.debug("Received an {} event for organization id {}", orgChange.getAction(), orgChange.getOrganizationId());
     }
 
 }
